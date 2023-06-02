e49b05000e27c35c26e8d35694155b9d0fb006d1
Pyon18Pyon <ayasayshi@gmail.com>
2023-06-02 16:00:31 +1000

    feat: add contents in contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+'use client';
+
+import styles from './contact.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
 
 const ContactPage = () => {
   return (
-    <h1>ContactPage</h1>
-  )
-}
+    <>
+      <style global jsx>
+        {`
+          html,
+          body {
+            background: #a1c462;
+          }
+        `}
+      </style>
+      <p className={styles.container}>
+        <h1 className={styles.title}>TO GET IN TOUCH</h1>
+      </p>
+      <div className={styles.container}>
+        <p className={styles.email}>aya@iamserious.com</p>
+      </div>
+      <div className={styles.container}>
+        <Link href="https://www.linkedin.com/in/ayako-bennett" target="_blank">
+          <Image
+            src="/images/linkedin.png"
+            width={30}
+            height={30}
+            alt="linkedin image"
+            className={styles.icons}
+          />
+        </Link>
+        <Link href="https://github.com/PyonAU" target="_blank">
+          <Image
+            src="/images/github.png"
+            width={30}
+            height={30}
+            alt="github image"
+            className={styles.icons}
+          />
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default ContactPage;
