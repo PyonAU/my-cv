804a7b231a577b448311222d8a87706df9ab4676
Pyon18Pyon <ayasayshi@gmail.com>
2023-06-05 16:29:49 +1000

    feat: add local storage functionality

--- app/projects/bookmark/bookmarkComponents/Modal.jsx
+++ app/projects/bookmark/bookmarkComponents/Modal.jsx
@@ -6,7 +6,8 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 // Validate Form
 function validate(nameValue, urlValue) {
-  const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+  const expression =
+    /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
   const regex = new RegExp(expression);
   if (!nameValue || !urlValue) {
     alert('Please submit values for both fields.');
@@ -48,7 +49,10 @@ const Modal = () => {
   const handleSubmit = (userInput) => {
     const nameValue = userInput.name;
 
-    if (!userInput.url.includes('http://') && !userInput.url.includes('https://')) {
+    if (
+      !userInput.url.includes('http://') &&
+      !userInput.url.includes('https://')
+    ) {
       userInput.url = `https://${userInput.url}`;
     }
     if (!validate(nameValue, userInput.url)) {
@@ -63,13 +67,24 @@ const Modal = () => {
     setInputData(newData);
     setInputField({
       name: '',
-      url: ''
+      url: '',
     });
   };
 
   const handleDelete = (url) => {
     setInputData(inputData.filter((data) => data.url !== url));
   };
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('bookmarks'));
+    if (items) {
+      setInputData(items);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('bookmarks', JSON.stringify(inputData));
+  }, [inputData]);
 
   return (
     <div>
