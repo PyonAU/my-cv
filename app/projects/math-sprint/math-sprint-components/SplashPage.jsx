1e5580d42cbd3bdb5cfe923fa4f83da6698e9775
Pyon18Pyon <ayasayshi@gmail.com>
2023-06-06 17:46:01 +1000

    feat: add splash page

---
+++ app/projects/math-sprint/math-sprint-components/SplashPage.jsx
@@ -0,0 +1,60 @@
+import styles from './SplashPage.module.css';
+import cx from 'classnames';
+
+const questionsRadioInput = [10, 25, 50, 99];
+
+const SplashPage = ({
+  selectQuestionAmount,
+  questionCount,
+  startRound,
+  isStarted,
+}) => {
+  const selectedContainer = cx(styles.radioContainer, {
+    [styles.selectedLabel]: true,
+  });
+
+  return (
+    <div className={styles.card} hidden={isStarted}>
+      <form onClick={selectQuestionAmount}>
+        {questionsRadioInput.map((number) => {
+          return (
+            <div className={styles.selectionContainer} key={number}>
+              {/* Questions Radio Input */}
+              <div
+                className={
+                  questionCount === number
+                    ? selectedContainer
+                    : styles.radioContainer
+                }
+              >
+                <label
+                  className={styles.question}
+                  htmlFor={`value-${number}`}
+                >{`${number} Questions`}</label>
+                <input
+                  type="radio"
+                  name="questions"
+                  value={number}
+                  id={`value-${number}`}
+                  className={styles.questionAmount}
+                />
+                <span className={styles.bestScore}>
+                  <span>Best Score</span>
+                  <span className={styles.bestScoreValue}></span>
+                </span>
+              </div>
+            </div>
+          );
+        })}
+        {/* Start Button */}
+        <div className={styles.selectionFooter}>
+          <button className={styles.start} type="submit" onClick={startRound}>
+            Start Round
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SplashPage;
