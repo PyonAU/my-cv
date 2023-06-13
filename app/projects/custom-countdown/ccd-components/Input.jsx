9423439e46785fa8fb677e71bd76fb3e64df3dea
Pyon18Pyon <ayasayshi@gmail.com>
2023-06-14 04:02:41 +1000

    feat: add countdown timer

--- app/projects/custom-countdown/ccd-components/Input.jsx
+++ app/projects/custom-countdown/ccd-components/Input.jsx
@@ -1,33 +1,72 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './Input.module.css';
 
-const timer = [
-  'Days',
-  'Hours',
-  'Minutes',
-  'Seconds'
-];
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 const Input = () => {
   // State
   const [userInput, setUserInput] = useState('');
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  // Variables
+  let distance = 0;
+  let countdownTimer;
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setUserInput({
       title: event.target[0].value,
-      date: event.target[1].value
-    }); 
+      date: event.target[1].value,
+    });
   };
 
-  console.log('userInput:', userInput);
+  console.log('userInput.date:', userInput.date);
+
+  // Calculate days, hours, minutes and seconds
+  const calculateTimeLeft = () => {
+    if (userInput.date) {
+      // Get the current time
+      const currentTime = new Date().getTime();
+      console.log('currentTime:', currentTime);
+
+      // Get the user input time
+      const userInputTime = new Date(userInput.date).getTime();
+      console.log('userInputTime:', userInputTime);
+
+      // Get the time remaining until the countdown date
+      distance = userInputTime - currentTime;
+
+      const days = Math.floor(distance / day);
+      const hours = Math.floor((distance % day) / hour);
+      const minutes = Math.floor((distance % hour) / minute);
+      const seconds = Math.floor((distance % minute) / second);
+
+      // Set the setTime state to each new time
+      setTime({ days: days, hours: hours, minutes: minutes, seconds: seconds });
+    }
+  };
+
+  useEffect(() => {
+    countdownTimer = setInterval(() => calculateTimeLeft(), second);
+  }, [userInput]);
 
   return (
     <div>
       {/* Container */}
       <div className={styles.container}>
         {/* Input */}
-        <div className={styles.inputContainer} hidden>
+        <div
+          className={styles.inputContainer}
+          hidden={userInput.date !== undefined ? true : false}
+        >
           <h1 className={styles.bigTitle}>Create a Custom Countdown!</h1>
           <form className={styles.form} onSubmit={handleSubmit}>
             <label className={styles.labels} htmlFor="title">
@@ -49,12 +88,18 @@ const Input = () => {
         </div>
 
         {/* Countdown */}
-        <div className={styles.countdown}>
-          <h1 className={styles.bigTitle}>Dad's birthday</h1>
+        <div
+          className={styles.countdown}
+          hidden={userInput.date === undefined || distance < 0 ? true : false}
+        >
+          <h1 className={styles.bigTitle}>{userInput.title}</h1>
           <ul className={styles.timer}>
-            {timer.map((value) => {
+            {Object.entries(time).map(([key, value], i) => {
               return (
-                <li className={styles.dateAndTime} key={value}>{value}</li>
+                <li className={styles.dateAndTime} key={i}>
+                  <span>{value}</span>
+                  {key}
+                </li>
               );
             })}
           </ul>
