ce61689f6700e1c735df6423fdf58268d42c4d2d
Pyon18Pyon <ayasayshi@gmail.com>
2023-06-14 01:43:33 +1000

    feat: add input functionality

--- app/projects/custom-countdown/ccd-components/Input.jsx
+++ app/projects/custom-countdown/ccd-components/Input.jsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import styles from './Input.module.css';
 
 const Input = () => {
+  // State
+  const [userInput, setUserInput] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUserInput({
+      title: event.target[0].value,
+      date: event.target[1].value
+    }); 
+  };
+
+  console.log('userInput:', userInput);
+
   return (
     <div>
       {/* Container */}
@@ -8,17 +22,27 @@ const Input = () => {
         {/* Input */}
         <div className={styles.inputContainer}>
           <h1 className={styles.bigTitle}>Create a Custom Countdown!</h1>
-          <form className={styles.form}>
-            <label className={styles.labels} htmlFor="title">Title</label>
-            <input className={styles.inputField} type="text" placeholder="What are you counting down to?" />
-            <label className={styles.labels} htmlFor="date-picker">Select a Date</label>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <label className={styles.labels} htmlFor="title">
+              Title
+            </label>
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="What are you counting down to?"
+            />
+            <label className={styles.labels} htmlFor="date-picker">
+              Select a Date
+            </label>
             <input className={styles.inputField} type="date" />
-            <button className={styles.submitButton} type="submit">Submit</button>
+            <button className={styles.submitButton} type="submit">
+              Submit
+            </button>
           </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Input;
