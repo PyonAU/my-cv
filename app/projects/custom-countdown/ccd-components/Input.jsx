57f119a93e11fa04f2a7ffec94b34053d6a59cc2
Pyon18Pyon <ayasayshi@gmail.com>
2023-06-14 02:01:41 +1000

    feat: add countdown display

--- app/projects/custom-countdown/ccd-components/Input.jsx
+++ app/projects/custom-countdown/ccd-components/Input.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
 import styles from './Input.module.css';
 
+const timer = [
+  'Days',
+  'Hours',
+  'Minutes',
+  'Seconds'
+];
+
 const Input = () => {
   // State
   const [userInput, setUserInput] = useState('');
@@ -20,7 +27,7 @@ const Input = () => {
       {/* Container */}
       <div className={styles.container}>
         {/* Input */}
-        <div className={styles.inputContainer}>
+        <div className={styles.inputContainer} hidden>
           <h1 className={styles.bigTitle}>Create a Custom Countdown!</h1>
           <form className={styles.form} onSubmit={handleSubmit}>
             <label className={styles.labels} htmlFor="title">
@@ -35,10 +42,23 @@ const Input = () => {
               Select a Date
             </label>
             <input className={styles.inputField} type="date" />
-            <button className={styles.submitButton} type="submit">
+            <button className={styles.buttons} type="submit">
               Submit
             </button>
           </form>
+        </div>
+
+        {/* Countdown */}
+        <div className={styles.countdown}>
+          <h1 className={styles.bigTitle}>Dad's birthday</h1>
+          <ul className={styles.timer}>
+            {timer.map((value) => {
+              return (
+                <li className={styles.dateAndTime} key={value}>{value}</li>
+              );
+            })}
+          </ul>
+          <button className={styles.buttons}>Reset</button>
         </div>
       </div>
     </div>
