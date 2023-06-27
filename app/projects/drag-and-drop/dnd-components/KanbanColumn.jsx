a17fc8d8f37fe4ae8957faf739ca97b5f4386077
Pyon18Pyon <ayasayshi@gmail.com>
2023-06-27 17:33:38 +1000

    feat: added add item buttons

--- app/projects/drag-and-drop/dnd-components/KanbanColumn.jsx
+++ app/projects/drag-and-drop/dnd-components/KanbanColumn.jsx
@@ -1,16 +1,24 @@
-import { labelsMap } from "@app/projects/lib/drop-and-drag/kanbanLists";
+import { labelsMap } from '@app/projects/lib/drop-and-drag/kanbanLists';
 import styles from './KanbanColumn.module.css';
-import cx from "classnames";
+import cx from 'classnames';
 
 const KanbanColumn = ({ status, style }) => {
-    // Style
-    const column = cx(styles.dragColumn, styles[style]);
+  // Style
+  const column = cx(styles.dragColumn, styles[style]);
 
   return (
     <li className={column}>
       <span className={styles.header}>
         <h1>{labelsMap[status]}</h1>
       </span>
+
+      {/* Add Button Group */}
+      <div className={styles.addBtnWrapper}>
+        <div className={styles.addBtn}>
+          <span className={styles.plusSign}>+</span>
+          <span>Add Item</span>
+        </div>
+      </div>
     </li>
   );
 };
