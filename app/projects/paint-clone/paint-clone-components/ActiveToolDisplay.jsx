9d5625e79e7b9e3a0ea7e6b00fda9c206cf2e7c5
Pyon18Pyon <ayasayshi@gmail.com>
2023-07-10 22:32:15 +1000

    feat: added other texts for other icons

--- app/projects/paint-clone/paint-clone-components/ActiveToolDisplay.jsx
+++ app/projects/paint-clone/paint-clone-components/ActiveToolDisplay.jsx
@@ -1,10 +1,23 @@
 import styles from './ActiveToolDisplay.module.css';
 
-const ActiveToolDisplay = ({ brushIcon }) => {
+const ActiveToolDisplay = ({ brushIcon, isCleared, save, load, clear }) => {
+  const tool = brushIcon ? 'Brush' : 'Eraser';
+
+  let text;
+  if (save) {
+    text = 'Canvas Saved';
+  } else if (load) {
+    text = 'Canvas Loaded';
+  } else if (clear) {
+    text = 'Local Storage Cleared';
+  } else if (isCleared) {
+    text = 'Canvas Cleared';
+  }
+
   return (
     <div>
       <span className={styles.activeTool} title="Active Tool">
-        {brushIcon ? 'Brush' : 'Eraser'}
+        {save || load || clear || isCleared ? text : tool}
       </span>
     </div>
   );
