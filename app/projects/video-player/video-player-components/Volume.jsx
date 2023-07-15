59acd2e19a6feead29b430de229980c053e281db
Pyon18Pyon <ayasayshi@gmail.com>
2023-07-15 22:29:54 +1000

    feat: added volume range

--- app/projects/video-player/video-player-components/Volume.jsx
+++ app/projects/video-player/video-player-components/Volume.jsx
@@ -1,20 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
 // import { faVolumeXmark } from '@fortawesome/free-solid-svg-icons';
+import Slider from '@mui/material/Slider';
 import styles from './Volume.module.css';
 
 const Volume = () => {
   return (
     <div className={styles.volume}>
       <div className={styles.volumeIcon}>
-      <FontAwesomeIcon className={styles.volumeIcons} icon={faVolumeHigh} title="Volume" />
+        <FontAwesomeIcon
+          className={styles.volumeIcons}
+          icon={faVolumeHigh}
+          title="Volume"
+        />
       </div>
-        
+
       <div className={styles.volumeRange}>
-        <p>Slider</p>
+        <Slider min={0} max={100} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Volume;
