4adaa32ee3db1c5b9403533e6da6ec00677573b2
Pyon18Pyon <ayasayshi@gmail.com>
2023-07-15 22:18:30 +1000

    feat: added progress bar Using PrettoSlider component to create a slide

--- app/projects/video-player/video-player-components/ProgressBar.jsx
+++ app/projects/video-player/video-player-components/ProgressBar.jsx
@@ -1,9 +1,12 @@
+import { PrettoSlider } from './PrettoSlider';
 import styles from './ProgressBar.module.css';
 
 const ProgressBar = () => {
   return (
-    <div className={styles.progressRange} title="Seek">ProgressBar</div>
-  )
-}
+    <div className={styles.progressRange} title="Seek">
+      <PrettoSlider min={0} max={100} defaultValue={20} />
+    </div>
+  );
+};
 
 export default ProgressBar;
